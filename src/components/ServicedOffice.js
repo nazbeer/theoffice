6107d9c5c009609079c2a19e0f92f158b7ba41f7
Nasbeer Ahammed <5653844+nazbeer@users.noreply.github.com>
2023-06-11 14:27:43 +0400

    Update ServicedOffice.js

--- src/components/ServicedOffice.js
+++ src/components/ServicedOffice.js
@@ -8,14 +8,14 @@ const ServicedOffice = () => {
   <div className='row row-serviced px-3 '>
    <div className=' d-flex justify-content-between align-items-center '>
     <div className='col-md-2  '>
-      <img src={leftimg} alt="office square" className='img-responsive s-leftcustomimg' />
+      <img src={leftimg} alt="office square" className='img-responsive ' />
 
     </div>
     <div className='col-md-8 text-left'>
         <p className='text-description p-4'>We are the premier choice for individuals and businesses seeking <span className='servicedofc-text'>serviced office</span> solutions. Within our prestigious locations, we also offer both exclusive and shared <span className='servicedofc-text'>co-working spaces</span>, designed with you in mind. We create an environment where <span className='servicedofc-text'>productivity</span> thrives and <span className='servicedofc-text'>success</span> becomes inevitable.  </p>
     </div>
     <div className='col-md-2 '>
-      <img src={rightimg} alt="office square" className='s-rightimg'/>
+      <img src={rightimg} alt="office square" className=''/>
 
     </div>
    </div>
